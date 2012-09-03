a48421f932e8838486365084fd6df4352e79feaa
Nicholas Devenish <ndevenish@gmail.com>
2012-09-03 17:33:50 +0100

    Fix the example json data

--- jsonTests/jsonTests.m
+++ jsonTests/jsonTests.m
@@ -116,7 +116,7 @@
 
 - (void)testRFCExample2
 {
-  NSString *testData = @"[{\"precision\":\"zip\",\"Latitude\":37.7668,\"Longitude\":-122.3959,\"Address\":\"\",\"City\":\"SANFRANCISCO\",\"State\":\"CA\",\"Zip\":\"94107\",\"Country\":\"US\"},{\"precision\":\"zip\",\"Latitude\":37.371991,\"Longitude\":-122.026020,\"Address\":\"\",\"City\":\"SUNNYVALE\",\"State\":\"CA\",\"Zip\":\"94085\",\"Country\":\"US\"}]";
+  NSString *testData = @"[{\"precision\":\"zip\",\"Latitude\":37.7668,\"Longitude\":-122.3959,\"Address\":\"\",\"City\":\"SAN FRANCISCO\",\"State\":\"CA\",\"Zip\":\"94107\",\"Country\":\"US\"},{\"precision\":\"zip\",\"Latitude\":37.371991,\"Longitude\":-122.026020,\"Address\":\"\",\"City\":\"SUNNYVALE\",\"State\":\"CA\",\"Zip\":\"94085\",\"Country\":\"US\"}]";
   NSArray *array = [[[NDJSONParser alloc] initWithString:testData] parse];
   NSArray *example = @[
    @{
